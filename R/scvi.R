bdc242f8eedadedb4139760d91a7fc240ac1ae9a
vjcitn <stvjc@channing.harvard.edu>
2025-05-09 06:04:09 -0400

    first draft

---
+++ R/scvi.R
@@ -0,0 +1,16 @@
+
+#' basic interface
+#' @importFrom reticulate import
+#' @return basiliskRun result with import from reticulate, typically a Module
+#' @examples
+#' scvi <- scviR()
+#' scvi
+#' scvi$model
+#' @export
+scviR <- function() {
+  proc <- basilisk::basiliskStart(bsklenv, testload="anndata")
+  on.exit(basilisk::basiliskStop(proc))
+  basilisk::basiliskRun(proc, function() {
+  reticulate::import("scvi")
+  })
+}
